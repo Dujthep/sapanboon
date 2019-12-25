ddcbad1e63c32afc84f0891db3040f4bac58bbdf
dujthep <dujthep@dujtheps-MacBook-Pro.local>
2019-12-25 16:42:50 +0700

    list project.

--- lib/sapanboon_web/controllers/projects_controller.ex
+++ lib/sapanboon_web/controllers/projects_controller.ex
@@ -5,8 +5,8 @@ defmodule SapanboonWeb.ProjectsController do
   alias Sapanboon.Project.Projects
 
   def index(conn, _params) do
-    aaa = Project.list_project()
-    render(conn, "index.html", aaa: aaa)
+    list_project = Project.list_project()
+    render(conn, "index.html", list_project: list_project)
   end
 
 end
