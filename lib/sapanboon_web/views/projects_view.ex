adf0d74de73e49389af8e0b810f62390c46393d1
dujthep <dujthep@dujtheps-MacBook-Pro.local>
2020-01-10 11:36:43 +0700

    fix highlight nav-bar.

--- lib/sapanboon_web/views/projects_view.ex
+++ lib/sapanboon_web/views/projects_view.ex
@@ -1,3 +1,14 @@
 defmodule SapanboonWeb.ProjectsView do
   use SapanboonWeb, :view
+
+  def format_date_time(date) do
+    {date.year, date.month, date.day}
+    |> Calendar.Strftime.strftime!("%A %d %B", :th)
+    |> Kernel.<>(" #{date.year + 543}")
+  end
+
+  def active_class(conn, path) do 
+    # IO.inspect(conn.params["status"])
+    if conn.params["status"] == path, do: "nav-link text-large active", else: "nav-link text-large"
+  end
 end
