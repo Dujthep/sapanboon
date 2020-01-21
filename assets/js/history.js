1b6fe0e060157f632c80b549eb4df10669407c05
Sothon Sitsiwalai <v_sothons@ctlsd.set>
2020-01-21 14:28:51 +0700

    change modal event

--- assets/js/history.js
+++ assets/js/history.js
@@ -3,42 +3,26 @@ window.jQuery = $;
 window.$ = $;
 
 $(document).ready(function () {
-  $("#confirm-modal").on("show.bs.modal", function(event) {
-    $('.modal-body #confirm').show();
-    $('.modal-body #error').hide();
-    const button = $(event.relatedTarget)
-    const modal = $(this)
-    modal.find(".modal-body input").val(button.data('whatever'))
+  $("#view-slip").on("click", function () {
+    $('.modal-body #img').attr("src", $(this).attr("data"));
+  })
+  $('.modal-body #error').hide();
+  $(".modal #btn-cancel-trans").on("click", function() {
+    $(".loader").addClass("is-active");
+    const transId = $('#cancel-trans').attr("data-payload")
+    // $.ajax({
+    //   url: '/cancel_trans',
+    //   data: { transactionId: transId },
+    //   type: 'POST',
+    //   dataType: 'json',
+    //   success: function (data) {
+    //     location.reload();
+    //     $(".loader").removeClass("is-active");
+    //   },
+    //   error: function (data) {
+    //     $('.modal-body #confirm').hide();
+    //     $('.modal-body #error').show();
+    //   }
+    // })
   })
 })
-
-// function viewSlip(imgSlip) {
-//   $('.modal-body #img').attr("src", imgSlip);
-// }
-
-function cancelTrans() {
-  const transId = $(".modal-body input").val();
-  $(".loader").addClass("is-active");
-  $.ajax({
-    url: '/cancel_trans',
-    data: { transactionId: transId },
-    type: 'POST',
-    dataType: 'json',
-    success: function (data) {
-      location.reload();
-      $(".loader").removeClass("is-active");
-    },
-    error: function (data) {
-      $('.modal-body #confirm').hide();
-      $('.modal-body #error').show();
-    }
-  })
-}
-
-
-window.onload = function () {
-  var mb = document.getElementById("view-slip");
-  mb.addEventListener("click", function(){
-      $('.modal-body #img').attr("src", $(this).attr("data"));
-  })
-}
