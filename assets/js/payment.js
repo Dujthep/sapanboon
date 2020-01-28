dfb1899fe8698004205c4564ad8b6219dafde5bb
kanin@odds.team <kanin@odds.team>
2020-01-28 22:03:24 +0700

    Add QR Code in payment.

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -1,24 +1,13 @@
 import { PromptPayQR } from './qrcode/promptpay-qr'
-import $ from 'jquery'
-window.jQuery = $
-window.$ = $
+import { QRCode } from './qrcode/qrcode'
 
 document.addEventListener('DOMContentLoaded', function() {
   generateQR()
 })
 
 function generateQR() {
-  // PromptPayQR.
+  var qr_dom = $('#qrcode')
+  var txt = PromptPayQR.gen_text('0955198387', 5) // Number 5 is amount.
+  qr_dom.innerHTML = ''
+  new QRCode(qrcode, txt)
 }
-
-
-// $(document).ready(function () {
-//   var qr_dom = $("#qrcode")
-//    var txt = PromptPayQR.gen_text('0994000001711', "test");
-//   qr_dom.innerHTML = "";
-//   qr = new QRCode(qrcode, txt)
-//   /*if(txt){
-//     new QRCode(qr_dom, txt);
-//   }*/
-
-// })
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = (env, options) => ({
     'detail': ['./js/detail.js'].concat(glob.sync('./vendor/**/*.js')),
     'payment': ['./js/payment.js'].concat(glob.sync('./vendor/**/*.js')),
     'promptpay-qr': ['./js/qrcode/promptpay-qr.js'].concat(glob.sync('./vendor/**/*.js')),
-    'qrcode': ['./js/qrcode/qrcode.min.js'].concat(glob.sync('./vendor/**/*.js'))
+    'qrcode': ['./js/qrcode/qrcode.js'].concat(glob.sync('./vendor/**/*.js'))
   },
   output: {
     filename: '[name].js',
