a14dbb74e7e87eea20ae276e7c87e674eff72bd1
dujthep <dujthep@dujtheps-MacBook-Pro.local>
2020-01-29 11:04:56 +0700

    create session for store user when login.

--- lib/sapanboon_web/router.ex
+++ lib/sapanboon_web/router.ex
@@ -7,6 +7,7 @@ defmodule SapanboonWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug SapanboonWeb.Plugs.SetUser
   end
 
   pipeline :api do
