0e995485970264b4d7777c33820348dcc1b4ad54
dujthep <dujthep@dujtheps-MacBook-Pro.local>
2020-01-31 15:53:55 +0700

    add share line

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -3,49 +3,59 @@ window.jQuery = $;
 window.$ = $;
 
 document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("line-icon").addEventListener("click", shareLine);
 
 });
 
-  $(document).ready(function() {
 
-    $('#donator-form').validate({
-      rules : {
-        inputAmount: {
-          required: true
-        }
-      },
-      messages: {
-        inputAmount: {
-          required: "โปรดระบุจำนวนเงิน",
-          maxlength: "โปรดระบุจำนวนเงินไม่เกิด 7 หลัก"
-        }
-      }
+function shareLine() {
+  const lineIcon = document.getElementById('line-icon')
+  const id = lineIcon.getAttribute("data-id");
+  const title = lineIcon.getAttribute("data-title");
+  var src =  window.location.origin + "/detail/" + id;
+  window.open('https://social-plugins.line.me/lineit/share/ui?' + 'text=' + 'ขอเชิญร่วมบริจาคโครงการ ' + title + ' ตามรายละเอียดด้านล่างนี้' + '&url=' + src);
+}
+
+$(document).ready(function() {
+
+    // $('#donator-form').validate({
+    //   rules : {
+    //     inputAmount: {
+    //       required: true
+    //     }
+    //   },
+    //   messages: {
+    //     inputAmount: {
+    //       required: "โปรดระบุจำนวนเงิน",
+    //       maxlength: "โปรดระบุจำนวนเงินไม่เกิด 7 หลัก"
+    //     }
+    //   }
+    // });
+
+  $('input.number').keyup(function(event) {
+    // skip for arrow keys
+    if(event.which >= 37 && event.which <= 40) return;
+
+    // format number
+    $(this).val(function(index, value) {
+      const valueReplace = value.split(",").join("");
+      return valueReplace > 0 ? value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",") : '';
     });
-
-    $('input.number').keyup(function(event) {
-      // skip for arrow keys
-      if(event.which >= 37 && event.which <= 40) return;
-
-      // format number
-      $(this).val(function(index, value) {
-        const valueReplace = value.split(",").join("");
-        return valueReplace > 0 ? value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",") : '';
-      });
-    });
-
-    $(document).on('click', 'input[name="amount"]', function() {
-      $('input[name=inputAmount]').removeClass('error')
-      if($(this).val() === "on") {
-        $("#input-free-style").show();
-        $("#label-free-style").hide();
-        $("#input-amount").focus();
-      } else {
-        $("#input-free-style").hide();
-        $("#label-free-style").show();
-        $("#input-amount").val('');
-      }
-    })
   });
+
+  $(document).on('click', 'input[name="amount"]', function() {
+    $('input[name=inputAmount]').removeClass('error')
+    if($(this).val() === "on") {
+      $("#input-free-style").show();
+      $("#label-free-style").hide();
+      $("#input-amount").focus();
+    } else {
+      $("#input-free-style").hide();
+      $("#label-free-style").show();
+      $("#input-amount").val('');
+    }
+  })
+});
 
   // $("#amount-error").hide()
   // $('#line-shared').attr('data-url',window.location.href);
@@ -66,3 +76,5 @@ document.addEventListener("DOMContentLoaded", function() {
   //     $(".visa-form").show();
   //   }
   // });
+
+
