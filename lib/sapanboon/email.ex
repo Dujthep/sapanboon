805061d9999b49c1cf28e94f110bfeabf503dba2
Sothon Sitsiwalai <v_sothons@ctlsd.set>
2020-02-04 17:07:18 +0700

    add template mail payment

--- lib/sapanboon/email.ex
+++ lib/sapanboon/email.ex
@@ -3,17 +3,27 @@ defmodule Sapanboon.Email do
 
   defp base_email do
     new_email()
-    |> from("Sapanboon")
-    |> put_html_layout({SapanboonWeb.LayoutView, "email.html"})
+      |> from("Sapanboon")
+      |> put_html_layout({SapanboonWeb.LayoutView, "email.html"})
   end
 
   def send_email_payment(email_address) do
+    mailUser = %{
+      projectName: "โครงการทดสอบ",
+      email: email_address,
+      fullName: "Sothon Sitsiwalai",
+      amount: "1,000",
+      paymentType: "Prompay",
+      transactionNo: "123",
+      day: "10", month: "มกราคม", year: "2563", time: "10:00",
+      project_id: "1"
+    }
     base_email()
-    |> to(email_address)
-    |> from("Sapanboon")
-    |> subject("ยืนยันการบริจาค")
-    |> assign(:email_address, email_address)
-    |> render("payment.html")
+      |> to(email_address)
+      |> from("Sapanboon")
+      |> subject("ยืนยันการบริจาค")
+      |> assign(:mailUser, mailUser)
+      |> render("payment.html")
   end
 
 end
