9219663c8ec28929826b4fae616dbd40e1cac501
kanin@odds.team <kanin@odds.team>
2020-03-04 19:17:44 +0700

    clone cal history

--- lib/sapanboon_web/controllers/projects_controller.ex
+++ lib/sapanboon_web/controllers/projects_controller.ex
@@ -34,8 +34,11 @@ defmodule SapanboonWeb.ProjectsController do
 
   def detail(conn, %{"id" => id}) do
     projects = Project.get_projects!(id)
-    histories = Histories.list_histories_by_projectId(projects.projectId)
-    transSum = Histories.sum_history(projects.projectId)
+    # histories = Histories.list_histories_by_projectId(projects.projectId)
+    # transSum = Histories.sum_history(projects.projectId)
+
+    histories = 0
+    transSum = 0
 
     render(conn, "detail.html", projects: projects, histories: histories, transSum: transSum)
   end
