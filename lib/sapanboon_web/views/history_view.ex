c159a12082daf3261690c9aea68f3acf94b05e95
kanin@odds.team <kanin@odds.team>
2020-03-04 22:19:14 +0700

    Add comma in history amound

--- lib/sapanboon_web/views/history_view.ex
+++ lib/sapanboon_web/views/history_view.ex
@@ -13,4 +13,17 @@ defmodule SapanboonWeb.HistoryView do
       title: history.name
     }
   end
+
+  def format_number(number) do
+    number
+    |> to_string
+    |> String.replace(~r/\d+(?=\.)|\A\d+\z/, fn int ->
+      int
+      |> String.graphemes()
+      |> Enum.reverse()
+      |> Enum.chunk_every(3, 3, [])
+      |> Enum.join(",")
+      |> String.reverse()
+    end)
+  end
 end
