0d63bd34b14521832915299b437a0124b7b5fd53
dujthep <dujthep@dujtheps-MacBook-Pro.local>
2020-03-10 23:01:25 +0700

    create shaere view

---
+++ lib/sapanboon_web/helper/share_helper.ex
@@ -0,0 +1,14 @@
+defmodule SapanboonWeb.ShareHelper do
+  def format_number(number) do
+    number
+    |> to_string
+    |> String.replace(~r/\d+(?=\.)|\A\d+\z/, fn int ->
+      int
+      |> String.graphemes()
+      |> Enum.reverse()
+      |> Enum.chunk_every(3, 3, [])
+      |> Enum.join(",")
+      |> String.reverse()
+    end)
+  end
+end
