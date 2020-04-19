63c76c667f62a775d1eab8824abadb9ff4c4fe1c
Sothon Sitsiwalai <v_sothons@ctlsd.set>
2020-04-19 23:06:34 +0700

    add test error_view

--- lib/sapanboon_web/views/error_view.ex
+++ lib/sapanboon_web/views/error_view.ex
@@ -1,9 +1,6 @@
 defmodule SapanboonWeb.ErrorView do
   use SapanboonWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-
   def render("500.html", _assigns) do
     render("500_page.html",%{})
   end
@@ -11,13 +8,6 @@ defmodule SapanboonWeb.ErrorView do
   def render("404.html", _assigns) do
     render("404_page.html",%{})
   end
-
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
-  # def template_not_found(template, _assigns) do
-  #   Phoenix.Controller.status_message_from_template(template)
-  # end
 
   def render("404.json", _assigns) do
     %{errors: %{message: "Page not found"}}
--- test/sapanboon_web/views/error_view_test.exs
+++ test/sapanboon_web/views/error_view_test.exs
@@ -1,3 +1,34 @@
 defmodule SapanboonWeb.ErrorViewTest do
   use SapanboonWeb.ConnCase, async: true
+  alias SapanboonWeb.ErrorView
+  import Phoenix.View
+
+  test "renders 500.html" do
+    errpr_page = render_to_string(ErrorView, "500.html", [])
+    assert errpr_page =~ "ไม่พบหน้าที่คุณต้องการ (404)"
+    assert errpr_page =~ "คุณพระ!"
+  end
+
+  test "renders 404.html" do
+    errpr_page = render_to_string(ErrorView, "404.html", [])
+    assert errpr_page =~ "ไม่พบหน้าที่คุณต้องการ (404)"
+    assert errpr_page =~ "คุณพระ!"
+  end
+
+  test "renders 404.json" do
+    expected = %{errors: %{message: "Page not found"}}
+    assert render(ErrorView, "404.json", []) == expected
+  end
+
+  test "renders 422.json" do
+    expected = %{errors: %{message: "Bad request", errors: %{view_module: SapanboonWeb.ErrorView, view_template: "422.json"}}}
+    assert render(ErrorView, "422.json", []) == expected
+  end
+
+  test "renders 500.json" do
+    expected = %{errors: %{message: "Internal server error"}}
+    assert render(ErrorView, "500.json", []) == expected
+  end
+
+
 end
