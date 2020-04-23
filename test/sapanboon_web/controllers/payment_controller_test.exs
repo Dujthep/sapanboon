80bcfe1e468f8fcd51a9751b56962392ba2dcf40
Sothon Sitsiwalai <v_sothons@ctlsd.set>
2020-04-23 17:42:13 +0700

    add test case success and refactor payment

--- test/sapanboon_web/controllers/payment_controller_test.exs
+++ test/sapanboon_web/controllers/payment_controller_test.exs
@@ -1,13 +1,10 @@
 defmodule SapanboonWeb.PaymentControllerTest do
   use SapanboonWeb.ConnCase
-  alias Sapanboon.SapanboonWeb
-
     describe "index" do
     test "update payment and get history", %{conn: conn} do
-      conn = get(conn, Routes.payment_path(conn, :index, 58))
-      item = conn.assigns.history
-
-      assert item.name =~ "บสถและพระประธ"
+      conn = get(conn, Routes.payment_path(conn, :index, 50))
+      assert html_response(conn, 200) =~ "กรุณาแสกน QR เพื่อบริจาค"
+      assert html_response(conn, 200) =~ "โครงการปล่อยปลาดุกหน้าเขียง วันที่ 1 มีนาคม 2563"
     end
   end
 end
