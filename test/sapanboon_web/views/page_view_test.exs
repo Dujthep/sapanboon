bf8462a57b1a470ef6f6abceba1d2da95c599bc4
Sothon Sitsiwalai <sothon@odds.team>
2020-04-29 22:37:01 +0700

    add test meta tag should show description, image and title

--- test/sapanboon_web/views/page_view_test.exs
+++ test/sapanboon_web/views/page_view_test.exs
@@ -1,3 +1,22 @@
 defmodule SapanboonWeb.PageViewTest do
   use SapanboonWeb.ConnCase, async: true
+  alias SapanboonWeb.PageView
+
+  test "meta_tags attrs_list, should have [description, image, title]" do
+    expected = [
+      {:safe, [60, "meta", [[32, "content", 61, 34, "ขอเชิญท่านผู้มีจิตศรัทธาร่วมสร้างสถานปฎิบัติธรรม", 34],
+      [32, "name", 61, 34, "og:description", 34]], 62]},
+      {:safe, [60, "meta", [[32, "content", 61, 34, "minio", 34],
+      [32, "property", 61, 34, "og:image", 34]], 62]},
+      {:safe, [60, "meta", [[32, "content", 61, 34, "สร้างสถานปฎิบัติธรรม สมเด็จพระสังฆราช (อมฺพรมหาเถร) วัดราชบพิธ", 34],
+      [32, "name", 61, 34, "og:title", 34]], 62]}
+    ]
+
+    attrs_list = [
+      %{name: "og:description", content: "ขอเชิญท่านผู้มีจิตศรัทธาร่วมสร้างสถานปฎิบัติธรรม"},
+      %{property: "og:image", content: "minio"},
+      %{name: "og:title", content: "สร้างสถานปฎิบัติธรรม สมเด็จพระสังฆราช (อมฺพรมหาเถร) วัดราชบพิธ"}
+    ]
+    assert PageView.meta_tags(attrs_list) == expected
+  end
 end
