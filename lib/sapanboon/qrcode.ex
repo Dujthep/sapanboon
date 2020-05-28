6a4c10d0d04eabacd2464bd5cddf03d98a1e6d3e
Kaewta Sueyanyongsiri <kaewta@odds.team>
2020-05-28 18:43:31 +0700

    [Phx-001][Kaew] create api generate QR code from SCB and use in payment controller

--- lib/sapanboon/qrcode.ex
+++ lib/sapanboon/qrcode.ex
@@ -3,6 +3,38 @@ defmodule Sapanboon.QRcode do
     token = getSCBToken()["data"]["tokenType"] <> " " <> getSCBToken()["data"]["accessToken"]
     IO.puts(token)
 
+    uuid = Ecto.UUID.generate()
+    url = "https://api-sandbox.partners.scb/partners/sandbox/v1/payment/qrcode/create"
+    body = "{
+      \"qrType\": \"PP\",
+      \"ppType\": \"BILLERID\",
+      \"ppId\": \"633153421253554\",
+      \"amount\": #{:amount},
+      \"ref1\": #{:transactionId},
+      \"ref2\": #{:projectCode},
+      \"ref3\": \"VJO\"
+    }"
+    headers = %{
+      "Content-Type": "application/json",
+      authorization: token,
+      resourceOwnerId: "l726d98af10f0b446da083bfbfed746c3c",
+      requestUId: uuid,
+      "accept-language": "EN",
+    }
+    case HTTPoison.post(url, body, headers) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        body
+        |> Poison.decode!()
+
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts("Not found :(")
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+        IO.inspect(reason)
+    end
+
+
 
 
 
@@ -17,7 +49,6 @@ defmodule Sapanboon.QRcode do
       \"applicationKey\": \"l726d98af10f0b446da083bfbfed746c3c\",
       \"applicationSecret\": \"4de6cbeace2b4d5b94b62bdb645e6bcd\"
     }"
-
     headers = %{
       "Content-Type": "application/json",
       resourceOwnerId: "l726d98af10f0b446da083bfbfed746c3c",
