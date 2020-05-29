4431a6f259aa2f5024bfb97037a0b33b1e6c80dd
Kaewta Sueyanyongsiri <kaewta@odds.team>
2020-05-29 11:28:36 +0700

    [Phx-001][Kaew] Edit type body to json

--- lib/sapanboon/qrcode.ex
+++ lib/sapanboon/qrcode.ex
@@ -41,15 +41,15 @@ defmodule Sapanboon.QRcode do
 
     uuid = Ecto.UUID.generate()
     url = "https://api-sandbox.partners.scb/partners/sandbox/v1/oauth/token"
-    body = "{
-      \"applicationKey\": \"l726d98af10f0b446da083bfbfed746c3c\",
-      \"applicationSecret\": \"4de6cbeace2b4d5b94b62bdb645e6bcd\"
-    }"
+    body = %{
+      applicationKey: "l726d98af10f0b446da083bfbfed746c3c",
+      applicationSecret: "4de6cbeace2b4d5b94b62bdb645e6bcd"
+    }|> Poison.encode!()
 
     headers = %{
       "Content-Type": "application/json",
-      "resourceOwnerId": "l726d98af10f0b446da083bfbfed746c3c",
-      "requestUId": uuid,
+      resourceOwnerId: "l726d98af10f0b446da083bfbfed746c3c",
+      requestUId: uuid,
       "accept-language": "EN"
     }
 
